4805692721b221369e223cc25ed0e4edc7b91cef
darkwerty <64215154+darkwerty@users.noreply.github.com>
2026-07-08 18:54:52 +0200

    Add: insertionSort - submission-19

---
+++ Data Structures & Algorithms/insertionSort/submission-19.py
@@ -0,0 +1,31 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        length = len(pairs)
+
+        if length == 0:
+            return []
+
+        pair_changes = []
+        pair_changes.append(pairs.copy())
+
+        # We check each pair of the pairs array starting from the second element
+        # Then we look at the element to its left and see if its bigger than actual element
+        # If it is, we have to insert the lower pair there and push the rest to the right
+        for i in range(1, length):
+            if pairs[i].key < pairs[i-1].key:
+                posInsertion = 0
+                
+                while posInsertion < i and pairs[i].key >= pairs[posInsertion].key: posInsertion += 1
+
+                # We have found first element that is greater than the element that we have to insert
+                pairs[posInsertion:i+1] = [pairs[i]] + pairs[posInsertion:i]
+
+
+            pair_changes.append(pairs.copy())
+
+        return pair_changes
